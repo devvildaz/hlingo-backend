c862d5d0b336351cc14326619b5c3e30d7312868
devvildaz <91985728+devvildaz@users.noreply.github.com>
2022-11-19 09:06:32 -0500

    fix: load_dotenv()

--- src/utils/token.py
+++ src/utils/token.py
@@ -5,6 +5,9 @@ import jwt
 from src.api.v1.contracts.auth import ClassicLoginUser
 import codecs
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
