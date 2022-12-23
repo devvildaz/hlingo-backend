6fc7b83786d320c9f2386d9c44d0e5820d2041ef
Erick Palomino <erickp1502@gmail.com>
2022-12-22 19:41:45 -0500

    feat: prometheus demo

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /hololingo-back
 
 COPY ./requirements.txt ./requirements.txt
 
-#COPY ./.env ./.env
+COPY ./.env ./.env
 
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
 
--- main.py
+++ main.py
@@ -3,29 +3,36 @@ from fastapi import FastAPI
 from mangum import Mangum
 from starlette.middleware.sessions import SessionMiddleware
 from starlette_csrf import CSRFMiddleware
-from starlette_exporter import PrometheusMiddleware, handle_metrics, optional_metrics
 
 from src.core.db.db import init_database, shutdown_database
 from src.api.v1.auth import auth_router
 from src.api.v1.info import info_router
 from src.core.settings import settings
 from src.api.v1.lessons import lessons_router
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
 
 app = FastAPI()
 
-
+instrumentator=Instrumentator()
 app.add_middleware(SessionMiddleware, secret_key="some-random", https_only=True)
-app.add_middleware(
-  PrometheusMiddleware,
-  app_name=settings.app_name,
-  prefix=settings.app_name,
-  optional_metrics=[optional_metrics.response_body_size, optional_metrics.request_body_size],
-  group_paths=True,
-  buckets=[0.1, 0.25, 0.5, 1],
-  skip_paths=['/health'],
-  always_use_int_status=False)
-app.add_route("/metrics", handle_metrics)
-
+instrumentator.add(metrics.latency(buckets=(1, 2, 3,)))
+instrumentator.add(
+    metrics.request_size(
+        should_include_handler=True,
+        should_include_method=True,
+        should_include_status=True,
+        metric_namespace="request_size",
+        metric_subsystem="hlingo",
+    )
+).add(
+    metrics.response_size(
+        should_include_handler=True,
+        should_include_method=True,
+        should_include_status=True,
+        metric_namespace="response_size",
+        metric_subsystem="hlingo",
+    )
+)
 app.include_router(auth_router)
 app.include_router(info_router)
 
@@ -34,6 +41,7 @@ app.include_router(lessons_router)
 @app.on_event("startup")
 async def init_config():
     init_database()
+    instrumentator.instrument(app).expose(app)
 
 
 @app.on_event("shutdown")
@@ -41,3 +49,4 @@ async def shutdown():
     shutdown_database()
 
 handler = Mangum(app=app)
+
